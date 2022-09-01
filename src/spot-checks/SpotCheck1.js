85ce558cc6e695954a33a6086270f69a16dc7771
mariasahatra <112659234+mariasahatra@users.noreply.github.com>
2022-09-01 22:41:45 +0300

    Update SpotCheck1.js

--- src/spot-checks/SpotCheck1.js
+++ src/spot-checks/SpotCheck1.js
@@ -8,7 +8,24 @@ export default function SpotCheck1() {
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 1</h4>
       <div className="exercise" id="sc-1">
-        {/* your code here... */}
+        {/* import React, { useState } from "react"
+
+          export default function Exercise1() {
+            // your code here
+            const [counter, setCounter] = useState(0)
+
+            const increase = () => setCounter(counter + 1)
+            const decrease = () => setCounter(counter - 1)
+
+            return (
+              <div>
+                <p>{counter}</p>
+                <button onClick={decrease}>Decrease</button>
+                <button onClick={increase}>Increase</button>
+              </div>
+            )
+          }
+ */}
 
       </div>
     </div>
