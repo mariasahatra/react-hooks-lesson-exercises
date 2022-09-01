a5d82329926fffdfc9bf15fc4478c0079af5a52c
mariasahatra <112659234+mariasahatra@users.noreply.github.com>
2022-09-01 22:43:28 +0300

    Update SpotCheck2.js

--- src/spot-checks/SpotCheck2.js
+++ src/spot-checks/SpotCheck2.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export default function SpotCheck2() {
-  // your code here
+  // import React, { useState } from 'react'
+
+export default function SpotCheck2() {
+ 
+  const [isLoading, setIsLoading] = useState(true)
+  const [lesson, setLesson] = useState({ name: 'React Hooks', completed: false, started: false })
+
+  const updateLesson = key => setLesson({ ...lesson, [key]: true })
+
+  return (
+    <div>
+        {isLoading
+          ? <div>Page is loading...</div>
+          : (
+            <div>
+              <p>{lesson.name}</p>
+              <p>Lesson status: {lesson.completed ? 'Completed' : lesson.started ? 'In progress' : 'Not started'}</p>
+              <button onClick={() => updateLesson('started')}>Begin Lesson</button>
+              <button onClick={() => updateLesson('completed')}>Complete Lesson</button>
+            </div>
+        )}
+    </div>
+  )
+}
 
 
   return (
